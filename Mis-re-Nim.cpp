80cdee7cc1cc80bc69544680cceb15f01ccab19d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-08-04 01:24:20 +0900

    Update Mis-re-Nim.cpp

--- Mis-re-Nim.cpp
+++ Mis-re-Nim.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'misereNim' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY s as parameter.
+ */
+
+string misereNim(vector<int> s) {
+
+}
 
