c30336790baefffc0242ea86e57c220784753a14
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-07-25 07:25:19 +0900

    Update Mis-re-Nim.lua

--- Mis-re-Nim.lua
+++ Mis-re-Nim.lua
@@ -1,1 +1,11 @@
-Mis-re-Nim
+--
+-- Complete the 'misereNim' function below.
+--
+-- The function is expected to return a STRING.
+-- The function accepts INTEGER_ARRAY s as parameter.
+--
+
+function misereNim(s)
+    -- Write your code here
+
+end
